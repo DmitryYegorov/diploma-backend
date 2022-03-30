8dea9512a321e47da0f877bdaa9a3711b48a07ea
Dmitry Yegorov <dmitry.yegorov@effective-soft.com>
2022-03-30 21:40:28 +0300

    add getUserById and register functions

--- src/auth/abstractions/Request.ts
+++ src/auth/abstractions/Request.ts
@@ -1,4 +1,15 @@
+import * as Enum from "./Enum";
+
 export interface Login {
   email: string;
   password: string;
+}
+
+export interface CreateUserAccount {
+  email: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  password: string;
+  role?: Enum.Role;
 }
--- src/auth/abstractions/Service.ts
+++ src/auth/abstractions/Service.ts
@@ -1,6 +1,8 @@
 import * as Request from "./Request";
 import * as Response from "./Response";
+import { user } from "@prisma/client";
 
 export interface Service {
   login(req: Request.Login): Promise<Response.GetLoginData>;
+  register(req: Request.CreateUserAccount): Promise<user>;
 }
